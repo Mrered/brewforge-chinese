612ebbb71cb625fad30ac9e63d1499806716a8a0
Mrered Cio <34948506+Mrered@users.noreply.github.com>
2023-12-10 20:21:38 +0800

    更新了软件列表

---
+++ Casks/gopeed.rb
@@ -0,0 +1,24 @@
+cask "gopeed" do
+  version "1.5.1"
+  sha256 "1b7f0fe112a606eecbac38e71e8f5a1df282acd9aeb62cb0b993e431bece857a"
+
+  url "https://github.com/GopeedLab/gopeed/releases/download/v#{version}/Gopeed-v#{version}-macos.dmg",
+      verified: "github.com/GopeedLab/gopeed/"
+  name "Gopeed"
+  desc "支持所有平台的高速下载器"
+  homepage "https://gopeed.com/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+
+  app "Gopeed.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.gopeed",
+    "~/Library/Containers/com.gopeed",
+  ]
+end
